0d943dbc8a7eddf6d3b460e17b4d947785739665
Diogo Fernandes <diogo.fernandes@rarolabs.com.br>
2020-07-02 15:48:33 -0300

    Subir outras formas de fazer testes

--- TestApiRestAssured/src/main/java/br/ce/dfoliveira/rest/OlaMundoTest.java
+++ TestApiRestAssured/src/main/java/br/ce/dfoliveira/rest/OlaMundoTest.java
@@ -7,14 +7,33 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.*;
+
 public class OlaMundoTest {
     @Test
     public void testOlaMundo()
     {
-        Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+        Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
         Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
         Assert.assertTrue(response.statusCode() == 200);
         Assert.assertTrue("O status code deveria ser 200",response.statusCode() == 200);
         Assert.assertEquals(200, response.statusCode());
     }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured(){
+        Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+
+        get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+
+        given()
+        .when()
+                .get("http://restapi.wcaquino.me/ola")
+        .then()
+                .statusCode(200);
+    }
+
 }
